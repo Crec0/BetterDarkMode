4c9af5add4ad61fedae4ce7d81e615dd6f889587
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-03-11 17:01:24 -0800

    refactor: Null check for empty file. Fixes #4

--- src/main/java/betterdarkmode/util/Util.java
+++ src/main/java/betterdarkmode/util/Util.java
@@ -12,68 +12,61 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Util {
-    public static int getRGB(int r, int g, int b) {
-        return ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
-    }
+public final class Util {
+	public static int getRGB(int r, int g, int b) {
+		return ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
+	}
 
-    private static Path getSaveFile() {
-        try {
-            Path saveFile = FabricLoader
-                    .getInstance()
-                    .getConfigDir()
-                    .resolve("BetterDarkMode")
-                    .resolve("selected_color.conf");
-
-            if (!Files.exists(saveFile.getParent())) {
-                Files.createDirectory(saveFile.getParent());
-            }
-
-            if (!Files.exists(saveFile)) {
+	private static Path getSaveFile() {
+		try {
+			Path saveFile = FabricLoader.getInstance().getConfigDir().resolve("BetterDarkMode").resolve("selected_color.conf");
+			if (!Files.exists(saveFile)) {
+				Files.createDirectories(saveFile.getParent());
                 Files.createFile(saveFile);
+			}
+			return saveFile;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void readSaveFile() {
+		Path saveFile = Util.getSaveFile();
+
+		if (saveFile == null) {
+			BetterDarkMode.LOGGER.info("Save file doesn't exist and couldn't create it.");
+			return;
+		}
+
+		try (BufferedReader reader = Files.newBufferedReader(saveFile)) {
+			String line = reader.readLine();
+            if (line != null) {
+                Configs.SELECTED_COLOR = Integer.parseInt(line.trim());
             }
+			BetterDarkMode.LOGGER.info("Successfully read save file.");
+		} catch (IOException | NumberFormatException e) {
+			BetterDarkMode.LOGGER.error("Failed to read save file.");
+			e.printStackTrace();
+		}
+	}
 
-            return saveFile;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public static void writeSaveFile() {
+		Path saveFile = Util.getSaveFile();
+		if (saveFile == null) {
+			BetterDarkMode.LOGGER.info("Save file doesn't exist and couldn't create it.");
+			return;
+		}
+		try (BufferedWriter writer = Files.newBufferedWriter(saveFile)) {
+			writer.write(Integer.toString(Configs.SELECTED_COLOR));
+			BetterDarkMode.LOGGER.info("Successfully wrote save file.");
+		} catch (IOException e) {
+			BetterDarkMode.LOGGER.error("Failed to write save file.");
+			e.printStackTrace();
+		}
+	}
 
-    public static void readSaveFile() {
-        Path saveFile = Util.getSaveFile();
-
-        if (saveFile == null) {
-            BetterDarkMode.LOGGER.info("Save file doesn't exist and couldn't create it.");
-            return;
-        }
-
-        try (BufferedReader reader = Files.newBufferedReader(saveFile)) {
-            String line = reader.readLine();
-            Configs.SELECTED_COLOR = Integer.parseInt(line.trim());
-            BetterDarkMode.LOGGER.info("Successfully read save file.");
-        } catch (IOException | NumberFormatException e) {
-            BetterDarkMode.LOGGER.error("Failed to read save file.");
-            e.printStackTrace();
-        }
-    }
-
-    public static void writeSaveFile() {
-        Path saveFile = Util.getSaveFile();
-        if (saveFile == null) {
-            BetterDarkMode.LOGGER.info("Save file doesn't exist and couldn't create it.");
-            return;
-        }
-        try (BufferedWriter writer = Files.newBufferedWriter(saveFile)) {
-            writer.write(Integer.toString(Configs.SELECTED_COLOR));
-            BetterDarkMode.LOGGER.info("Successfully wrote save file.");
-        } catch (IOException e) {
-            BetterDarkMode.LOGGER.error("Failed to write save file.");
-            e.printStackTrace();
-        }
-    }
-
-    public static Text getCleanText(Text text) {
-        return new LiteralText(text.getString().replaceAll("§.", ""));
-    }
+	public static Text getCleanText(Text text) {
+		return new LiteralText(text.getString().replaceAll("§.", ""));
+	}
 }
