125a0641ce3c216be8280b703a40b03b406f68a7
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-07-27 22:35:45 -0700

    fix crash during runtime

--- src/main/java/dev/crec/betterdarkmode/mixin/compat/rei/LabelWidgetMixin.java
+++ src/main/java/dev/crec/betterdarkmode/mixin/compat/rei/LabelWidgetMixin.java
@@ -7,16 +7,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Pseudo
-@Mixin(targets = "me.shedaniel.rei.impl.client.gui.widget.basewidgets.LabelWidget", remap = false)
+@Mixin(targets = { "me.shedaniel.rei.impl.client.gui.widget.basewidgets.LabelWidget" }, remap = false)
 public class LabelWidgetMixin {
 	@ModifyArg(
-		method = "render",
+		method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/gui/Font;draw(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/util/FormattedCharSequence;FFI)I",
 			remap = true
 		),
-		remap = false,
+		remap = true,
 		index = 4
 	)
 	private int onRenderText(int i) {
@@ -25,13 +25,13 @@ public class LabelWidgetMixin {
 
 
 	@ModifyArg(
-		method = "render",
+		method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/gui/Font;drawShadow(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/util/FormattedCharSequence;FFI)I",
 			remap = true
 		),
-		remap = false,
+		remap = true,
 		index = 4
 	)
 	private int onRenderShadow(int i) {
